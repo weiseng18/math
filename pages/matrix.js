1c69d4457209a18289ad38d8367e663498e7022b
weiseng18 <tws.hci@gmail.com>
2021-08-04 14:34:49 +0800

    feat: create basic frontend page for determinant queries

---
+++ pages/matrix.js
@@ -0,0 +1,49 @@
+import { Button, Flex, HStack, Input, Text, VStack } from "@chakra-ui/react"
+import { useState } from "react"
+
+import axios from "axios"
+
+const Page = () => {
+  const [query, setQuery] = useState("")
+  const [answer, setAnswer] = useState("")
+
+  const handleChange = (e) => {
+    setQuery(e.target.value)
+  }
+
+  const handleSubmit = async () => {
+    if (query === "") return
+
+    const res = await axios.get("/api/matrix/determinant", {
+      params: {
+        matrix: query,
+      },
+    })
+
+    setAnswer(res.data)
+  }
+
+  return (
+    <Flex h="100vh" w="100vw" justifyContent="center" alignItems="center">
+      <VStack spacing={4} w="60%">
+        <HStack spacing={4} w="100%">
+          <Input
+            isRequired
+            onChange={handleChange}
+            placeholder="Put in your query"
+          />
+          <Button disabled={!query.length} onClick={handleSubmit}>
+            Submit
+          </Button>
+        </HStack>
+        {answer && (
+          <>
+            <Text>Output: {answer}</Text>
+          </>
+        )}
+      </VStack>
+    </Flex>
+  )
+}
+
+export default Page
