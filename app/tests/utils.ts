88ef2a1249bf4df8fbbaa9f9a4ce51fe58b597a5
weiseng18 <tws.hci@gmail.com>
2021-08-07 15:23:54 +0800

    test: create test for leadingEntryIndex

--- app/tests/utils.ts
+++ app/tests/utils.ts
@@ -3,7 +3,7 @@ import chai from "chai"
 
 // import utils to be tested
 import { range } from "../utils/misc"
-import { isZeroRow } from "../utils/Matrix"
+import { isZeroRow, leadingEntryIndex } from "../utils/Matrix"
 
 describe("Util tests", () => {
   describe("Misc: range", () => {
@@ -31,6 +31,27 @@ describe("Util tests", () => {
         const res = isZeroRow(arr)
         chai.expect(res).to.equal(false)
       })
+    })
+  })
+
+  describe("Matrix: leadingEntryIndex", () => {
+    it("should return index of the first nonzero entry", () => {
+      const arrays = [[1, 2, 3], [0, 0, 1], [1]]
+      const ans = [0, 2, 0]
+
+      arrays.forEach((arr, idx) => {
+        const res = leadingEntryIndex(arr)
+        chai.expect(res).to.equal(ans[idx])
+      })
+    })
+    it("should return -1 for zero arrays", () => {
+      const res = range(5).every((val) => {
+        // create zero array with length val+1
+        const tmp = []
+        for (let i = 0; i < val + 1; i++) tmp.push(0)
+        return leadingEntryIndex(tmp) === -1
+      })
+      chai.expect(res).to.equal(true)
     })
   })
 })
