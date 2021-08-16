6204723465c160de2b2efd4645e73c1abe25d3f9
weiseng18 <tws.hci@gmail.com>
2021-08-16 21:10:29 +0800

    test: add tests for non-rectangular matrices (either missing or extra data)

--- app/tests/index.ts
+++ app/tests/index.ts
@@ -116,6 +116,34 @@ describe("API tests", () => {
             .to.equal("Row and column counts do not match")
         })
     })
+    it("should obtain 400 for 3 by 3 matrix with extra data", async () => {
+      // the backend treats this as a 3 by 3 matrix
+      // since it takes the number of elements in the 1st row as the number of columns
+      await chai
+        .request(url)
+        .get(route)
+        .query({
+          matrix: "[[1,0,0],[0,4,0,1,2],[0,0,2]]",
+        })
+        .then((res) => {
+          chai.expect(res.status).to.equal(400)
+          chai.expect(res.body.message).to.equal("Matrix is not rectangle")
+        })
+    })
+    it("should obtain 400 for 3 by 3 matrix with missing data", async () => {
+      // the backend treats this as a 3 by 3 matrix
+      // since it takes the number of elements in the 1st row as the number of columns
+      await chai
+        .request(url)
+        .get(route)
+        .query({
+          matrix: "[[1,0,0],[0,4],[0,0]]",
+        })
+        .then((res) => {
+          chai.expect(res.status).to.equal(400)
+          chai.expect(res.body.message).to.equal("Matrix is not rectangle")
+        })
+    })
   })
 
   describe("/api/matrix/inverse", () => {
@@ -161,6 +189,34 @@ describe("API tests", () => {
             .to.equal("Matrix is singular; No inverse exists")
         })
     })
+    it("should obtain 400 for 3 by 3 matrix with extra data", async () => {
+      // the backend treats this as a 3 by 3 matrix
+      // since it takes the number of elements in the 1st row as the number of columns
+      await chai
+        .request(url)
+        .get(route)
+        .query({
+          matrix: "[[1,0,0],[0,4,0,1,2],[0,0,2]]",
+        })
+        .then((res) => {
+          chai.expect(res.status).to.equal(400)
+          chai.expect(res.body.message).to.equal("Matrix is not rectangle")
+        })
+    })
+    it("should obtain 400 for 3 by 3 matrix with missing data", async () => {
+      // the backend treats this as a 3 by 3 matrix
+      // since it takes the number of elements in the 1st row as the number of columns
+      await chai
+        .request(url)
+        .get(route)
+        .query({
+          matrix: "[[1,0,0],[0,4],[0,0]]",
+        })
+        .then((res) => {
+          chai.expect(res.status).to.equal(400)
+          chai.expect(res.body.message).to.equal("Matrix is not rectangle")
+        })
+    })
   })
 
   describe("/api/matrix/rref", () => {
@@ -176,6 +232,34 @@ describe("API tests", () => {
         })
         .then((res) => {
           chai.expect(res.status).to.equal(200)
+        })
+    })
+    it("should obtain 400 for 3 by 1 matrix with extra data", async () => {
+      // the backend treats this as a 3 by 1 matrix
+      // since it takes the number of elements in the 1st row as the number of columns
+      await chai
+        .request(url)
+        .get(route)
+        .query({
+          matrix: "[[1],[3,4],[5,6]]",
+        })
+        .then((res) => {
+          chai.expect(res.status).to.equal(400)
+          chai.expect(res.body.message).to.equal("Matrix is not rectangle")
+        })
+    })
+    it("should obtain 400 for 3 by 2 matrix with missing data", async () => {
+      // the backend treats this as a 3 by 2 matrix
+      // since it takes the number of elements in the 1st row as the number of columns
+      await chai
+        .request(url)
+        .get(route)
+        .query({
+          matrix: "[[1,2],[3],[5]]",
+        })
+        .then((res) => {
+          chai.expect(res.status).to.equal(400)
+          chai.expect(res.body.message).to.equal("Matrix is not rectangle")
         })
     })
   })
